c46e27dec3ba19375ecf556970d16e9be8115867
Spencer Daniels <speencah@spencer-danielss-macbook-pro.local>
2018-06-15 00:27:19 -0600

    added herokuu connection informaion

--- server.js
+++ server.js
@@ -42,7 +42,12 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/mongoScraper");
+// mongoose.connect("mongodb://localhost/mongoScraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 var db = mongoose.connection;
 
 // Show any mongoose errors
